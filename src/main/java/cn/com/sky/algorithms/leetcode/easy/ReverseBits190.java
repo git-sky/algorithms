6e1fb1804afe189a4eaa338342e23fdb39f91ef5
git-sky <linkme2008@sina.com>
2026-07-07 09:17:46 +0800

    add

--- src/main/java/cn/com/sky/algorithms/leetcode/easy/ReverseBits190.java
+++ src/main/java/cn/com/sky/algorithms/leetcode/easy/ReverseBits190.java
@@ -1,56 +1,56 @@
-package cn.com.sky.algorithms.leetcode.easy;
-
-import org.junit.Test;
-
-/**
- * <pre>
- * 
- * 190. Reverse Bits
- * 
- * Reverse bits of a given 32 bits unsigned integer.
- * 
- * For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
- * 
- * Follow up:
- * If this function is called many times, how would you optimize it?
- * 
- * Related problem: Reverse Integer
- * 
- * </pre>
- */
-public class ReverseBits190 {
-
-	@Test
-	public void solution() {
-
-		int n = 43261596;
-
-		int count = reverseBits(n);
-		System.out.println(count);
-	}
-
-	public int reverseBits(int n) {
-		int result = 0;
-		for (int i = 0; i < 32; i++) {
-			result += n & 1;
-			n >>>= 1; // CATCH: must do unsigned shift
-			if (i < 31) // CATCH: for last digit, don't shift!
-				result <<= 1;
-		}
-		return result;
-	}
-
-	public int reverseBits(int n) {
-		int result = 0;
-		for (int i = 0; i < 32; i++) {
-			result <<= 1;
-			result += n & 1;
-			n >>= 1;
-		}
-		return result;
-	}
-
-	public int reverseBits(int n) {
-		return Integer.reverse(n);
-	}
-}
+//package cn.com.sky.algorithms.leetcode.easy;
+//
+//import org.junit.Test;
+//
+///**
+// * <pre>
+// *
+// * 190. Reverse Bits
+// *
+// * Reverse bits of a given 32 bits unsigned integer.
+// *
+// * For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+// *
+// * Follow up:
+// * If this function is called many times, how would you optimize it?
+// *
+// * Related problem: Reverse Integer
+// *
+// * </pre>
+// */
+//public class ReverseBits190 {
+//
+//	@Test
+//	public void solution() {
+//
+//		int n = 43261596;
+//
+//		int count = reverseBits(n);
+//		System.out.println(count);
+//	}
+//
+//	public int reverseBits(int n) {
+//		int result = 0;
+//		for (int i = 0; i < 32; i++) {
+//			result += n & 1;
+//			n >>>= 1; // CATCH: must do unsigned shift
+//			if (i < 31) // CATCH: for last digit, don't shift!
+//				result <<= 1;
+//		}
+//		return result;
+//	}
+//
+//	public int reverseBits(int n) {
+//		int result = 0;
+//		for (int i = 0; i < 32; i++) {
+//			result <<= 1;
+//			result += n & 1;
+//			n >>= 1;
+//		}
+//		return result;
+//	}
+//
+//	public int reverseBits(int n) {
+//		return Integer.reverse(n);
+//	}
+//}
--- src/main/java/cn/com/sky/algorithms/leetcode/easy/TwoSum1.java
+++ src/main/java/cn/com/sky/algorithms/leetcode/easy/TwoSum1.java
@@ -8,67 +8,102 @@ import org.junit.Test;
 
 /**
  * <pre>
- * 
+ *
  * 1. Two Sum
- * 
+ *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * 
+ *
  * Example:
  * Given nums = [2, 7, 11, 15], target = 9,
- * 
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
- * 
+ *
  * </pre>
  */
 public class TwoSum1 {
 
-	@Test
-	public void solution() {
-		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
+    @Test
+    public void solution() {
+        int[] nums = {5, 3, 2, 1, 7, 11, 15, 5};
+        int target = 9;
 
-		int[] sum = twoSum(nums, target);
-		System.out.println(Arrays.toString(sum));
-	}
+        int[] sum1 = twoSum1(nums, target);
+        int[] sum2 = twoSum2(nums, target);
+        int[] sum3 = twoSum3(nums, target);
+        System.out.println(Arrays.toString(sum1));
+        System.out.println(Arrays.toString(sum2));
+        System.out.println(Arrays.toString(sum3));
+    }
 
-//	public int[] twoSum(int[] nums, int target) {
-//		Map<Integer, Integer> map = new HashMap<>();
-//		for (int i = 0; i < nums.length; i++) {
-//			map.put(nums[i], i);
-//		}
-//		for (int i = 0; i < nums.length; i++) {
-//			int complement = target - nums[i];
-//			if (map.containsKey(complement) && map.get(complement) != i) {
-//				return new int[] { i, map.get(complement) };
-//			}
-//		}
-//		throw new IllegalArgumentException("No two sum solution");
-//	}
-//
-//	public int[] twoSum(int[] nums, int target) {
-//		Map<Integer, Integer> map = new HashMap<>();
-//		for (int i = 0; i < nums.length; i++) {
-//			int complement = target - nums[i];
-//			if (map.containsKey(complement)) {
-//				return new int[] { map.get(complement), i };
-//			}
-//			map.put(nums[i], i);
-//		}
-//		throw new IllegalArgumentException("No two sum solution");
-//	}
+    /**
+     * 方法一：HashMap两次遍历法
+     * 时间复杂度：O(n)，空间复杂度：O(n)
+     * 思路：先将所有元素及其索引存入HashMap，然后遍历数组查找补数
+     *
+     * @param nums   整数数组
+     * @param target 目标和
+     * @return 两个数的索引，使得它们的和等于target
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 
-	public int[] twoSum(int[] nums, int target) {
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[j] == target - nums[i]) {
-					return new int[] { i, j };
-				}
-			}
-		}
-		throw new IllegalArgumentException("No two sum solution");
-	}
+    //
+//	/**
+//	 * 方法二：HashMap一次遍历法（最优解）
+//	 * 时间复杂度：O(n)，空间复杂度：O(n)
+//	 * 思路：遍历数组时同时查找补数，找到即返回，未找到则将当前元素存入map
+//	 * 优点：只需一次遍历，效率更高
+//	 * 
+//	 * @param nums   整数数组
+//	 * @param target 目标和
+//	 * @return 两个数的索引，使得它们的和等于target
+//	 */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 方法三：暴力枚举法
+     * 时间复杂度：O(n²)，空间复杂度：O(1)
+     * 思路：双重循环遍历所有元素组合，找到和为target的两个数
+     * 优点：无需额外空间
+     * 缺点：效率较低，适合小规模数据
+     *
+     * @param nums   整数数组
+     * @param target 目标和
+     * @return 两个数的索引，使得它们的和等于target
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 
 }
